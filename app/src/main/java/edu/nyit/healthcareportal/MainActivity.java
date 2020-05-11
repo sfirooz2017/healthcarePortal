8dce01f58ef0e4a86361eb3565da480e81b96b1a
Shannon Firooz <msfirooz@yahoo.com>
2020-05-11 13:02:59 -0400

    removed floating action button

--- app/src/main/java/edu/nyit/healthcareportal/MainActivity.java
+++ app/src/main/java/edu/nyit/healthcareportal/MainActivity.java
@@ -52,14 +52,6 @@ public class MainActivity extends AppCompatActivity {
 
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
-        FloatingActionButton fab = findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
         DrawerLayout drawer = findViewById(R.id.drawer_layout);
         NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment);
         NavGraph graph = navController.getNavInflater().inflate(R.navigation.mobile_navigation);
